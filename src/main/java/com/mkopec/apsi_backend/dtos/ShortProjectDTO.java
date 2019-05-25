88f94fb8aab430331353c534aeb15956fb662d34
mateuszkp96 <mateuszkp96@gmail.com>
2019-05-25 11:28:57 +0200

    Short project dto added

---
+++ src/main/java/com/mkopec/apsi_backend/dtos/ShortProjectDTO.java
@@ -0,0 +1,11 @@
+package com.mkopec.apsi_backend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ShortProjectDTO {
+    private Integer id;
+    private String name;
+}
