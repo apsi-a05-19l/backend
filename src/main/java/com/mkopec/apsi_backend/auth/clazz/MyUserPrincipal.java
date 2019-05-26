038c7fe544c3ce9c8f189b2c546089ca14a66fa0
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2019-05-26 17:08:18 +0200

    Auth

---
+++ src/main/java/com/mkopec/apsi_backend/auth/clazz/MyUserPrincipal.java
@@ -0,0 +1,53 @@
+package com.mkopec.apsi_backend.auth.clazz;
+
+import com.mkopec.apsi_backend.auth.domain.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+
+@AllArgsConstructor
+public class MyUserPrincipal implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        final List<GrantedAuthority> authorities = singletonList(new SimpleGrantedAuthority("User"));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/mkopec/apsi_backend/auth/domain/User.java
@@ -0,0 +1,20 @@
+package com.mkopec.apsi_backend.auth.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "User")
+public class User {
+    @Column(name = "Person_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+}
