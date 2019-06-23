d936915f5a5b171562ec7d2214be2258032e05ee
mateuszkp96 <mateuszkp96@gmail.com>
2019-06-23 12:26:27 +0200

    Save post with parts and links working

--- src/main/java/com/mkopec/apsi_backend/controller/PostController.java
+++ src/main/java/com/mkopec/apsi_backend/controller/PostController.java
@@ -1,16 +1,17 @@
 package com.mkopec.apsi_backend.controller;
 
 
-import com.mkopec.apsi_backend.domain.Part;
 import com.mkopec.apsi_backend.domain.Post;
 import com.mkopec.apsi_backend.dtos.FullPostDTO;
 import com.mkopec.apsi_backend.dtos.ShortPostDTO;
+import com.mkopec.apsi_backend.enums.PostTopic;
 import com.mkopec.apsi_backend.mapper.PostMapper;
 import com.mkopec.apsi_backend.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -34,11 +35,16 @@ public class PostController {
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public FullPostDTO postPost (@RequestBody FullPostDTO fullPostDTO) {
-     Post post = mapper.toPost(fullPostDTO);
-     return mapper.toFullPostDTO(postService.postPost(post));
+        Post post = mapper.toPost(fullPostDTO);
+        return mapper.toFullPostDTO(postService.postPost(post));
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public void deletePost(@PathVariable Integer id) { postService.deletePost(id); }
+
+    @GetMapping("/topic")
+    public List<PostTopic> getPostTopics(){
+        return Arrays.asList(PostTopic.values());
+    }
 }
--- src/main/java/com/mkopec/apsi_backend/service/PostService.java
+++ src/main/java/com/mkopec/apsi_backend/service/PostService.java
@@ -1,19 +1,26 @@
 package com.mkopec.apsi_backend.service;
 
 
+import com.mkopec.apsi_backend.domain.Link;
+import com.mkopec.apsi_backend.domain.Part;
 import com.mkopec.apsi_backend.domain.Post;
 import com.mkopec.apsi_backend.exception.ResourceNotFoundException;
+import com.mkopec.apsi_backend.repository.LinkRepository;
+import com.mkopec.apsi_backend.repository.PartRepository;
 import com.mkopec.apsi_backend.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository repository;
+    private final PartRepository partRepository;
+    private final LinkRepository linkRepository;
 
     public Post getSinglePost(Integer id) {
         return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
@@ -23,7 +30,25 @@ public class PostService {
 
     @Transactional
     public Post postPost(Post post) {
-        return repository.save(post);
+        List<Part> parts = post.getParts();
+        post.setParts(null);
+        Post newPost = repository.save(post);
+        newPost.setParts(savePostParts(parts, newPost));
+        return repository.save(newPost);
+    }
+
+    private List<Part> savePostParts(List<Part> parts, Post newPost) {
+        List<Part> newParts = new ArrayList<>();
+        parts.forEach(part -> {
+            part.setPost(newPost);
+            List<Link> links = part.getLinks();
+            part.setLinks(null);
+            Part newPart = partRepository.save(part);
+            links.forEach(link -> link.setPart(newPart));
+            newPart.setLinks(linkRepository.saveAll(links));
+            newParts.add(partRepository.save(newPart));
+        });
+        return newParts;
     }
 
     public void deletePost(Integer id) {repository.deleteById(id);}
